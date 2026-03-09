177fc49df3da1cb4c0f61246eab3bce21546d015
Henrique Horch <hhorchenrique@gmail.com>
2026-03-09 16:56:10 -0300

    opa

---
+++ src/main/java/com/example/signinLoginBackEnd/SigninLoginBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.signinLoginBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigninLoginBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigninLoginBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/signinLoginBackEnd/SigninLoginBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signinLoginBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigninLoginBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

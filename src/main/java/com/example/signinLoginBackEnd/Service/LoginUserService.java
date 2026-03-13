6b717bba2a3500b58a6b49877bd741b49f5ff34d
Henrique Horch <hhorchenrique@gmail.com>
2026-03-12 23:37:39 -0300

    login service done

--- src/main/java/com/example/signinLoginBackEnd/Service/LoginUserService.java
+++ src/main/java/com/example/signinLoginBackEnd/Service/LoginUserService.java
@@ -10,24 +10,23 @@ import com.example.signinLoginBackEnd.Repo.UserRepository;
 @Service
 public class LoginUserService {
 
+    @Autowired
+    UserRepository repo;
 
-@Autowired
-UserRepository repo;
+    public LoginResponse loginService(String email, String password) {
 
-
-public LoginResponse loginService(String email, String password) {
-
-    User userFound = repo.findByEmail(email);
-    if(userFound == null) {
-        return null;
+        User userFound = repo.findByEmail(email);
+        if (userFound == null) {
+            LoginResponse response = new LoginResponse(null, LoginResponse.ResponseCode.USUARIO_NAO_ENCONTRADO);
+            return response;
+        }
+        String expectedKey = userFound.getPassword();
+        if (password.equals(expectedKey)) {
+            LoginResponse response = new LoginResponse(userFound, LoginResponse.ResponseCode.SUCESSO);
+            return response;
+        }
+        LoginResponse response = new LoginResponse(null, LoginResponse.ResponseCode.SENHA_INCORRETA);
+        return response;
     }
-    String expectedKey = userFound.getPassword();
-    if(password.equals(expectedKey)) {
-        return userFound;
-    }
-return null;
-}
-
-
 
 }
